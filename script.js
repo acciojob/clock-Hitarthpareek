6862751b0c3418e3a8a7ba918b83223c1d2ed52f
Hitarthpareek <80564705+Hitarthpareek@users.noreply.github.com>
2025-11-11 11:39:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const updateTime = document.getElementById("timer")
+let currDate = new Date()
+setInterval(()=>{
+	currDate = new Date()
+	updateTime.innerHTML = currDate.toLocaleString();
+},1000)
